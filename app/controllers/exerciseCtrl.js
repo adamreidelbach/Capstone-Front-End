087deb8cff8f0135ac7022bef59fb82cec120a6b
Adam Reidelbach <adamreidelbach@gmail.com>
2017-06-20 16:01:51 -0500

    highlights working, append stopped

--- app/controllers/exerciseCtrl.js
+++ app/controllers/exerciseCtrl.js
@@ -11,6 +11,7 @@ app.controller('ExerciseCtrl', function(DataFactory, $scope, $routeParams, StepF
           $scope.getStep(0, $scope.instructions);
           StepFactory.setStepsNum(instructions.steps.length);
           $rootScope.steps = instructions.steps.length;
+          // $scope.addStyling($scope.instructions);
           return instructions;
         });
     };
@@ -22,7 +23,8 @@ app.controller('ExerciseCtrl', function(DataFactory, $scope, $routeParams, StepF
     $scope.getStep = (index, obj) => {
         $scope.currentStep = obj.steps[index];
         $rootScope.page = index;
-        $scope.getStyling($scope.currentStep);
+        // $scope.removeHighlight();
+        // $scope.addStyling($scope.currentStep);
     };
 
     $scope.nextStep = () => {
@@ -48,26 +50,37 @@ app.controller('ExerciseCtrl', function(DataFactory, $scope, $routeParams, StepF
             console.log("CORRECT");
             $("#inst1").prop("checked", true);
             $("#appendText").append($scope.currentStep.append1);
+            $scope.addHighlight($scope.currentStep.style1);
         } else if ($scope.currentStep.answer2.includes($scope.userAnswer) && $("#inst1").prop("checked") === true) {
             console.log("CORRECT");
             $("#inst2").prop("checked", true);
             $("#appendText").append($scope.currentStep.append2);
+            $scope.addHighlight($scope.currentStep.style2);
+            $scope.removeHighlight($scope.currentStep.removeStyle2);
         } else if ($scope.currentStep.answer3.includes($scope.userAnswer)) {
             console.log("CORRECT");
             $("#inst3").prop("checked", true);
             $("#appendText").append($scope.currentStep.append3);
+            $scope.addHighlight($scope.currentStep.style3);
+            $scope.removeHighlight($scope.currentStep.removeStyle3);
         } else if ($scope.currentStep.answer4.includes($scope.userAnswer)) {
             console.log("CORRECT");
             $("#inst4").prop("checked", true);
             $("#appendText").append($scope.currentStep.append4);
+            $scope.addHighlight($scope.currentStep.style4);
+            $scope.removeHighlight($scope.currentStep.removeStyle4);
         } else if ($scope.currentStep.answer5.includes($scope.userAnswer)) {
             console.log("CORRECT");
             $("#inst5").prop("checked", true);
             $("#appendText").append($scope.currentStep.append5);
+            $scope.addHighlight($scope.currentStep.style5);
+            $scope.removeHighlight($scope.currentStep.removeStyle5);
         } else if ($scope.currentStep.answer6.includes($scope.userAnswer)) {
             console.log("CORRECT");
             $("#inst6").prop("checked", true);
             $("#appendText").append($scope.currentStep.append6);
+            $scope.addHighlight($scope.currentStep.style6);
+            $scope.removeHighlight($scope.currentStep.removeStyle6);
         } else {
             console.log("INCORRECT");
             $scope.userAnswer = "";
@@ -80,56 +93,65 @@ app.controller('ExerciseCtrl', function(DataFactory, $scope, $routeParams, StepF
         newBox.addClass("highlight");
     };
 
-    $scope.getStyling = (currentStep) => {
-        switch($scope.userAnswer) {
-            case "git add .":
-                break;
-            case "git commit -m \"building splash page\"":
-                $("#addTerminalText").append($scope.currentStep.terminal1);
-                break;
-            case "git checkout -b JohnStyling":
-                $("#addTerminalText").append($scope.currentStep.terminal1);
-                $("#lb").addClass("highlight");
-                break;
-            case "git pull origin JohnStyling":
-                $("#lb").addClass("highlight");
-                break;
-            case "git checkout master":
-                $("#addTerminalText").append($scope.currentStep.terminal1);
-                $scope.styling($("#lb"), $("#lm"));
-                break;
-            case "git checkout myBranch":
-                $("#addTerminalText").append($scope.currentStep.terminal1);
-                $scope.styling($("#lm"), $("#lb"));
-                break;
-            case "git merge master":
-                $("#addTerminalText").append($scope.currentStep.terminal1);
-                break;
-            case "git status":
-                $("#addTerminalText").append($scope.currentStep.terminal1);
-                break;
-            case "git checkout -b myNavbar":
-                $("#addTerminalText").append($scope.currentStep.terminal1);
-                $("#lb").addClass("highlight");
-                break;
-            case "git commit -m \"completed the navbar\"":
-                $("#addTerminalText").append($scope.currentStep.terminal3);
-                console.log("terminal3", $scope.currentStep.terminal3);
-                break;
-            case "git push origin myNavbar":
-                $("#addTerminalText").append($scope.currentStep.terminal4);
-                $scope.styling($("#lb"), $("#gb"));
-                break;
-            case "git pull origin master":
-                $("#addTerminalText").append($scope.currentStep.terminal5);
-                $scope.styling($("#gb"), $("#lm"));
-                break;
-            case "git checkout -b myFooter":
-                $("#addTerminalText").append($scope.currentStep.terminal6);
-                $scope.styling($("#lm"), $("#lb"));
-                break;
-        }
+    $scope.removeHighlight = (currentStep) => {
+        console.log("removehighlight currentStep", currentStep);
+        let element = document.querySelector(currentStep);
+        console.log("element removehighlight", element);
+        element.classList.remove("highlight");
     };
+
+    $scope.addHighlight = (currentStep) => {
+        console.log("addHighlight currentStep", currentStep);
+        let element = document.querySelector(currentStep);
+        console.log("element addhighlight", element);
+        element.classList.add("highlight");
+    };
+
+
+    // $scope.addStyling = (currentInstructions) => {
+    //     console.log("currentInstructions in addStyling", currentInstructions);
+    //     //logic for tutorial 1
+    //     // if (currentInstructions.name === "Tutorial 1") {
+    //         console.log("you are in tut1");
+    //         console.log("querySelector", document.querySelector($scope.currentStep.style));
+    //         // if ($scope.userAnswer === "git checkout -b myNavbar") {
+    //         //     $("#addTerminalText").append($scope.currentStep.terminal1);
+    //         //     $("#lb").addClass("highlight");
+    //         // }
+    //         // if ($scope.userAnswer === "git commit -m \"completed the navbar\"") {
+    //         //     $("#addTerminalText").append($scope.currentStep.terminal3);
+    //         //     console.log("terminal3", $scope.currentStep.terminal3);
+    //         // }
+    //         // if ($scope.userAnswer === "git push origin myNavbar") {
+    //         //     $("#addTerminalText").append($scope.currentStep.terminal4);
+    //         //     $scope.styling($("#lb"), $("#gb"));
+    //         // }
+    //         // if ($scope.userAnswer === "git pull origin master") {
+    //         //     $("#addTerminalText").append($scope.currentStep.terminal5);
+    //         //     $scope.styling($("#gb"), $("#lm"));
+    //         // }
+    //         // if ($scope.userAnswer === "git checkout -b myFooter") {
+    //         //     $("#addTerminalText").append($scope.currentStep.terminal6);
+    //         //     $scope.styling($("#lm"), $("#lb"));
+    //         // }
+    //     // }
+    //     // if (currentInstructions.name === "Tutorial 2") {
+    //     //     console.log("you are in tut2");
+    //     //     if ($scope.userAnswer === "git status") {
+    //     //         $("#addTerminalText").append($scope.currentStep.terminal1);
+    //     //         $("#lb").addClass("highlight");
+    //     //         console.log("git has been statused");
+    //     //     }
+    //     //     if ($scope.userAnswer === "git checkout master") {
+    //     //         $("#addTerminalText").append($scope.currentStep.terminal1);
+    //     //         $scope.styling($("#lb"), $("#lm"));
+    //     //     }
+    //     //     if ($scope.userAnswer === "git checkout myBranch") {
+    //     //         $("#addTerminalText").append($scope.currentStep.terminal1);
+    //     //         $scope.styling($("#lm"), $("#lb"));
+    //     //     }
+    //     // }
+    // };
 
      // if statement for ensuring users complete answers in the appropriate order
      // if ($scope.userAnswer !== "" && ("#inst2").prop("chcked") === false) {
