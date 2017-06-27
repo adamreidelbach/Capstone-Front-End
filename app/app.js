40d6f5b4574b715a97715090b2badcfecd29b33e
Adam Reidelbach <adamreidelbach@gmail.com>
2017-06-27 14:42:03 -0400

    terminal kind of working, but not really

--- app/app.js
+++ app/app.js
@@ -1,12 +1,12 @@
 "use strict";
 
-const app = angular.module('GitApp', ["ngRoute", "ui.materialize", "angular-terminal"])
+const app = angular.module('GitApp', ["ngRoute", "ui.materialize", "angular-terminal"]);
 
-.run(function ($rootScope) {
-                $rootScope.$on('terminal.main', function (e, input, terminal) {
-                    $rootScope.$emit('terminal.main.echo', 'input received: ' + input);
-                });
-            });
+// .run(function ($rootScope) {
+//                 $rootScope.$on('terminal.main', function (e, input, terminal) {
+//                     $rootScope.$emit('terminal.main.echo', 'input received: ' + input);
+//                 });
+//             });
 
 app.config(function($routeProvider) {
     $routeProvider.
