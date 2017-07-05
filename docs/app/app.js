4b2ee2f6c6f6715502fbbf1ce2f036ebb0036754
Adam Reidelbach <adamreidelbach@gmail.com>
2017-07-05 13:01:34 -0500

    added docs folder

---
+++ docs/app/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const app = angular.module('GitApp', ["ngRoute", "ui.materialize"]);
+
+// .run(function ($rootScope) {
+//                 $rootScope.$on('terminal.main', function (e, input, terminal) {
+//                     $rootScope.$emit('terminal.main.echo', 'input received: ' + input);
+//                 });
+//             });
+
+app.config(function($routeProvider) {
+    $routeProvider.
+    when(`/`, {
+        templateUrl: 'partials/splash.html',
+        controller: 'SplashCtrl'
+    }).
+    when('/exercises/:id', {
+        templateUrl: 'partials/exercises.html',
+        controller: 'ExerciseCtrl'
+    }).
+    otherwise('/');
+
+});
